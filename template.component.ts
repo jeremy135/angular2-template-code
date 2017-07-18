33df297e755af2691984b34444aeeb8396550f06
Jeremy Jackson <blackgrow@yandex.ru>
2017-07-18 16:40:14 +0800

    component template

---
+++ template.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Componant({
+  selector: 'comp-sel',
+  templateUrl: './.html',
+  stylesUrl: ['./scss/']
+})
+
+export class Comp {
+  constructor() {
+  }
+}
