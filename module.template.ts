5d7008faebd299f844e9e14f2e5ef45a0dd61a1d
Jeremy Jackson <blackgrow@yandex.ru>
2017-07-21 16:03:05 +0800

    module template

---
+++ module.template.ts
@@ -0,0 +1,13 @@
+
+import { NgMOdule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@NgModule({
+  imports: [],
+  declarations: [],
+  entryComponents: [],
+  schemas: [],
+  exports: []
+})
+
+export class MOdule {}
