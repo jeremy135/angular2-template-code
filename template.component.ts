9e93c932cca043e55a3861570be3ff4d77c44417
Jeremy Jackson <blackgrow@yandex.ru>
2017-07-31 15:49:48 +0800

    Update template.component.ts

--- template.component.ts
+++ template.component.ts
@@ -2,11 +2,11 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'comp-sel',
-  templateUrl: './.html',
-  styleUrls: ['./scss/']
+  templateUrl: './component.html',
+  styleUrls: ['./scss/.scss']
 })
 
-export class Comp {
+export class Component {
   constructor() {
   }
 }
